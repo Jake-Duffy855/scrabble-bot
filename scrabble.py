789543ac0258f86a4e46810d5b1c9ef12f0a581b
Jake Duffy <duffyjemmett@gmail.com>
2021-04-07 15:07:03 -0400

    first commit

---
+++ scrabble.py
@@ -0,0 +1,38 @@
+with open("dictionary.txt") as d:
+    words = d.read()
+    words = words.split()
+
+values = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+          "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+          "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+          "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+          "x": 8, "z": 10}
+
+
+def is_valid(word: str):
+    """
+    Is the given word a valid scrabble word?
+    :param word: The word whose validity is to be determined
+    :return: True if the word is valid, False otherwise
+    :rtype: bool
+    """
+    return word.upper() in words
+
+
+def score_word(word: str):
+    """
+    Returns the score of the given word, 0 if invalid
+    :param word: the word to be scored
+    :return: the score of the word, 0 if invalid
+    :rtype: int
+    """
+    if is_valid(word):
+        score = 0
+        for c in word:
+            score += values[c]
+        return score
+    else:
+        return 0
+
+
+print(score_word("butts"))
