0abbdb18ce9192f53f404915cab04d8ce25a4560
Jake Duffy <duffyjemmett@gmail.com>
2021-04-09 22:34:00 -0400

    added text field

--- display.py
+++ display.py
@@ -5,15 +5,73 @@ import time
 pygame.init()
 pygame.font.init()
 font = pygame.font.SysFont("Arial", 16)
+COLOR_INACTIVE = pygame.Color('lightskyblue3')
+COLOR_ACTIVE = pygame.Color('dodgerblue2')
+FONT = pygame.font.Font(None, 16)
+clock = pygame.time.Clock()
 
 size = (450, 500)
 tile_size = 30
 cursor = [0, 0]
 submit_text = font.render("Submit", False, (255, 255, 255), (50, 50, 50))
-submit_button = submit_text.get_rect(center=(225, 475))
+submit_button = submit_text.get_rect(center=(400, 475))
 get_best_text = font.render("Get Best", False, (255, 255, 255), (50, 50, 50))
-get_best_button = get_best_text.get_rect(center=(50, 475))
+get_best_button = get_best_text.get_rect(center=(280, 475))
 letters = {}
+
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width() + 10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+
+    def get_text(self):
+        text = self.text
+        self.text = ""
+        return text
+
+    def is_active(self):
+        return self.active
+
+
+input_box = InputBox(20, 465, 40, 20)
 
 # Set up the drawing window
 screen = pygame.display.set_mode(size)
@@ -76,6 +134,9 @@ if __name__ == '__main__':
         render_board(my_board)
         render_controls()
 
+        input_box.update()
+        input_box.draw(screen)
+
         # Flip the display
         pygame.display.flip()
 
@@ -84,6 +145,8 @@ if __name__ == '__main__':
 
             if event.type == pygame.QUIT:
                 running = False
+
+            input_box.handle_event(event)
 
             # handle MOUSEBUTTONUP
 
@@ -96,7 +159,7 @@ if __name__ == '__main__':
                     cursor[1] -= 1
                 elif event.key == pygame.K_DOWN:
                     cursor[1] += 1
-                elif 97 <= event.key <= 122:
+                elif 97 <= event.key <= 122 and not input_box.is_active():
                     try:
                         letters[cursor[1], cursor[0]] = chr(event.key)
                     except ValueError:
@@ -111,13 +174,13 @@ if __name__ == '__main__':
                         print("Invalid Play")
                     letters = {}
                 elif get_best_button.collidepoint(event.pos):
+                    print("Running...")
                     t = time.time_ns()
-                    best = my_board.get_best_play(input("Your letters: "))
+                    best = my_board.get_best_play(input_box.get_text())
                     print(best[0], best[1])
                     print((time.time_ns() - t) / 1000000)
                     my_board.play(best[0])
-                else:
+                elif event.pos[1] <= 450:
                     cursor = [int(i / tile_size) for i in event.pos]
 
-            if event.type == pygame.QUIT:
-                pygame.quit()
+        clock.tick(30)
