af70010f7e448b950fb0901a5c7b9bfae582bdb0
Jake Duffy <duffyjemmett@gmail.com>
2021-05-09 23:49:45 -0400

    added delete of typed tiles on board, and added highlight of last played word.

--- display.py
+++ display.py
@@ -13,12 +13,13 @@ clock = pygame.time.Clock()
 
 size = (450, 500)
 tile_size = 30
-cursor = [0, 0]
+cursor = [7, 7]
 submit_text = font.render("Submit", False, (255, 255, 255), (50, 50, 50))
 submit_button = submit_text.get_rect(center=(400, 475))
 get_best_text = font.render("Get Best", False, (255, 255, 255), (50, 50, 50))
 get_best_button = get_best_text.get_rect(center=(280, 475))
 letters = {}
+last_play = {}
 
 # Set up the drawing window
 screen = pygame.display.set_mode(size)
@@ -113,10 +114,18 @@ def render_board(board: Board):
             pygame.draw.rect(screen, (0, 0, 0), (tile_size * col, tile_size * row, tile_size, tile_size), width=1)
 
     # cursor
+    add_highlight(cursor[0], cursor[1])
+
+    # highlight last play
+    for row, col in last_play:
+        add_highlight(col, row, color=(100, 250, 100))
+
+
+def add_highlight(x, y, color=(255, 220, 0)):
     cur = pygame.Surface((tile_size - 2, tile_size - 2))  # the size of your rect
     cur.set_alpha(150)  # alpha level
-    cur.fill((255, 220, 0))  # this fills the entire surface
-    screen.blit(cur, (tile_size * cursor[0] + 1, tile_size * cursor[1] + 1))
+    cur.fill(color)  # this fills the entire surface
+    screen.blit(cur, (tile_size * x + 1, tile_size * y + 1))
 
 
 def render_controls():
@@ -127,6 +136,7 @@ def render_controls():
 
 
 def get_best():
+    global last_play
     print("Running...")
     t = time.time_ns()
     text = input_box.get_text()
@@ -136,6 +146,7 @@ def get_best():
         print((time.time_ns() - t) / 1000000)
         try:
             my_board.play(best_play)
+            last_play = best_play.letters.copy()
         except ValueError:
             print("Invalid play")
 
@@ -144,28 +155,32 @@ def get_best():
 
 
 def handle_key(event):
-    if event.key == pygame.K_LEFT:
-        cursor[0] = max(0, cursor[0] - 1)
-    elif event.key == pygame.K_RIGHT:
-        cursor[0] = min(14, cursor[0] + 1)
-    elif event.key == pygame.K_UP:
-        cursor[1] = max(0, cursor[1] - 1)
-    elif event.key == pygame.K_DOWN:
-        cursor[1] = min(14, cursor[1] + 1)
-    elif event.key == pygame.K_RETURN and not input_box.is_active():
-        submit()
-    elif 97 <= event.key <= 122 and not input_box.is_active():
-        try:
-            letters[cursor[1], cursor[0]] = chr(event.key)
-        except ValueError:
-            pass
+    if not input_box.is_active():
+        if event.key == pygame.K_LEFT:
+            cursor[0] = max(0, cursor[0] - 1)
+        elif event.key == pygame.K_RIGHT:
+            cursor[0] = min(14, cursor[0] + 1)
+        elif event.key == pygame.K_UP:
+            cursor[1] = max(0, cursor[1] - 1)
+        elif event.key == pygame.K_DOWN:
+            cursor[1] = min(14, cursor[1] + 1)
+        elif event.key == pygame.K_RETURN:
+            submit()
+        elif event.key == pygame.K_DELETE or event.key == 8:
+            handle_delete()
+        elif 97 <= event.key <= 122:
+            try:
+                letters[cursor[1], cursor[0]] = chr(event.key)
+            except ValueError:
+                pass
 
 
 def submit():
-    global letters
+    global letters, last_play
     try:
         print(my_board.score_play(Play(letters)))
         my_board.play(Play(letters))
+        last_play = letters.copy()
     except ValueError:
         print("Invalid Play")
     letters = {}
@@ -179,6 +194,12 @@ def handle_button_up(event):
         get_best()
     elif event.pos[1] <= 450:
         cursor = [int(i / tile_size) for i in event.pos]
+
+
+def handle_delete():
+    row, col = cursor[1], cursor[0]
+    if letters.get((row, col), False):
+        letters.pop((row, col))
 
 
 if __name__ == '__main__':
